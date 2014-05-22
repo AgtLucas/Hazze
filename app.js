10ec02dfe2293d74b7399df662fc3cbf11fe0d9a
Lucas Churchill <lucas@agtlucas.com>
2014-05-21 22:41:35 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var Sequelize = require('sequelize');
+
+var seq = new Sequelize('database', 'user', 'pass');
+
+var project = seq.define('project', {
+  title:        Sequelize.STRING,
+  description:  Sequelize.TEXT
+});
+
+var task = seq.define('task', {
+  title:        Sequelize.STRING,
+  description:  Sequelize.TEXT,
+  deadline:     Sequelize.DATE
+});
+
+project.hasMany(task);
+
+// Create an instance
+var myTask = task.build({title: 'Much task'})
+// myTask.title
+
+myTask.save()
+  .error(function (err) {
+    console.log('Ops, we have a situation over here...' + err);
+  })
+  .success(function () {
+    console.log('Good news everyone!');
+  });
